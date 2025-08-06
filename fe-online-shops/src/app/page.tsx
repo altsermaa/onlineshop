51663532581c1eaffeb7ba54d8fb2a71733030a3
altsermaa <altsermaa@gmail.com>
2025-08-06 12:45:40 +0800

    fixed package-json

--- fe-online-shops/src/app/page.tsx
+++ fe-online-shops/src/app/page.tsx
@@ -1,16 +1,17 @@
-import axios from "axios";
 import { Header } from "./_components/Header";
 import { ShowCards } from "./_components/ShowCards";
 
 const Home = async () => {
-  const data = await axios.get("http://localhost:8000/getAllProducts");
-  console.log(data)
-
+  const response = await fetch("http://localhost:8000/getAllProducts", {
+    cache: "no-store"
+  });
+  const data = await response.json();
+  console.log(data);
 
   return (
     <div className="w-[360px] lg:w-[930px] mx-auto">
       {/* <Header /> */}
-      <ShowCards products={data.data.products} />
+      <ShowCards products={data.products} />
     </div>
   );
 }
